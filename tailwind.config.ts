606235bc4bec350f8c2b0d32fd08dc85a94734b8
haseakito <haseakito@gmail.com>
2023-08-08 22:33:57 +0800

    :wrench: Add animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,10 +9,22 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      animation: {
+        "scale-in-center": "scale-in-center 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) 3s  both",
+      },
+      keyframes: {
+        "scale-in-center": {
+            "0%": {
+                transform: "scale(0)",
+                opacity: "1"
+            },
+            to: {
+                transform: "scale(1)",
+                opacity: "1"
+            }
+        }, 
+      }
     },
   },
   plugins: [],
