cc583a5ec7121c9ad10484dee215f8d7c8e56179
haseakito <haseakito@gmail.com>
2023-08-08 23:49:51 +0800

    :sparkles: Add Form type

---
+++ utils/types/Form.ts
@@ -0,0 +1,12 @@
+// type for sign up form
+export type SignupFormProps = {    
+    email: string,
+    password: string,
+    confirmPassword: string
+}
+
+// type for login form
+export type LoginFormProps = {
+    email: string,
+    password: string
+}
